4da1227162bbd48e57a2c974ada32935b8672694
jamesya28 <149421292+jamesya28@users.noreply.github.com>
2023-11-03 00:42:20 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectitem(x) {
+    removeborder();
+    removeshow();
+    // Add border to current tab
+    this.classList.add('tab-border');
+    // Grab content item from DOM
+    // console.log(this.id);
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // Add show class
+    tabContentItem.classList.add('show');
+}
+
+function removeborder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+function removeshow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectitem));
